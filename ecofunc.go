68a9015aa6263697c75f283211b01477f7f3077d
LapshovAV <andrey.lapshov@myoffice.team>
2024-10-31 16:48:18 +0400

    add version

--- ecofunc.go
+++ ecofunc.go
@@ -4,3 +4,8 @@ package ecofunc
 func PresentValue(futureValue float64, rate float64, periods int) float64 {
 	return futureValue / (1 + rate)
 }
+
+// PresentValue calculates the present value of a future amount
+func Version() string {
+	return "1.0"
+}
